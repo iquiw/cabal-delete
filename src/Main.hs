d86d53f3e24c368681be70be635e0ba3596446a1
Iku Iwasa <iku.iwasa@gmail.com>
2010-04-26 23:46:59 +0900

    Add help option '-h', '--help' to show help.

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,27 @@
 module Main where
 
+import Data.Char
 import System.Console.GetOpt
 import System.Environment
 import System.Exit
+import System.IO
 
 import CabalDelete.Command
 import CabalDelete.Types
 
-data CDCmd = CmdList | CmdListMinor | CmdNoDeps | CmdCheck
+data CDCmd =
+      CmdHelp
+    | CmdList
+    | CmdListMinor
+    | CmdNoDeps
+    | CmdCheck
     deriving (Eq, Show)
 
 options :: [OptDescr CDCmd]
 options =
-    [ Option "l" ["multiple-versions"] (NoArg CmdList)
+    [ Option "h" ["help"] (NoArg CmdHelp)
+      "show this help"
+    , Option "l" ["multiple-versions"] (NoArg CmdList)
       "show list of packages that have multiple versions"
     , Option "m" ["multiple-minors"] (NoArg CmdListMinor)
       "show list of packages that have multiple minor versions"
@@ -23,24 +32,31 @@ options =
     ]
 
 usage :: String -> IO ()
-usage s = do
-    let shortUsage = "usage: cabal-delete [options] [packages]"
-    putStrLn $ usageInfo (unlines [shortUsage, s]) options
-    exitWith $ ExitFailure 1
+usage err = do
+    let us = "usage: cabal-delete [options] [packages]"
+        (msg, out, ecode) = if null err
+                            then (unlines [us], stdout, ExitSuccess)
+                            else (unlines [us, err], stderr, ExitFailure 1)
+    hPutStrLn out $ usageInfo msg options
+    exitWith ecode
 
 main :: IO ()
 main = do
     (opts, pkgs ,errs) <- getOpt RequireOrder options `fmap` getArgs
     case errs of
         [] -> case opts of
-            [] | null pkgs -> usage "specify option or package names"
-               | otherwise -> cmdDelete pkgs
-            [cmd]          -> doCmd cmd pkgs 
-            _              -> usage "specify exactly one option"
-        _  -> usage $ concat errs
+            [] | null pkgs           -> usage "specify option or package names"
+               | otherwise           -> cmdDelete pkgs
+            [cmd]                    -> doCmd cmd pkgs 
+            _  | CmdHelp `elem` opts -> doCmd CmdHelp []
+               | otherwise           -> usage "specify exactly one option"
+        _  -> usage $ chop $ concat errs
   where
-    doCmd CmdList _      = cmdList (==.)
+    doCmd CmdHelp _      = usage []
+    doCmd CmdList _      = cmdList (==.) 
     doCmd CmdListMinor _ = cmdList (.==)
     doCmd CmdNoDeps _    = cmdNoDeps
     doCmd CmdCheck []    = usage "specify package names"
     doCmd CmdCheck pkgs  = cmdCheck pkgs
+
+    chop = reverse . dropWhile isSpace . reverse
